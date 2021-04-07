3c8922de6831d03474520c049d122c3a8ff7305d
Maxim Rezvanov <maxim.rezvanov@iridescent.studio>
2021-04-07 19:03:27 +0300

    Initial commit

---
+++ Assets/Scripts/TrajectoryRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrajectoryRenderer : MonoBehaviour
+{
+
+    private LineRenderer lineRenderer;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+
+    }
+
+    public void ShowTrajectory(Vector3 origin, Vector3 speed)
+    {
+        Vector3[] points = new Vector3[100];
+        lineRenderer.positionCount = points.Length;
+
+        for(int i = 0; i < points.Length; i++)
+        {
+            float time = i * 0.1f;
+            points[i] = origin + speed * time + Physics.gravity * time * time / 2;
+
+            if(points[i].y < 0)
+            {
+                lineRenderer.positionCount = i + 1;
+                break;
+            }
+        }
+
+        lineRenderer.SetPositions(points);
+
+    }
+
+
+}
